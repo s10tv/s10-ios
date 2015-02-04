eca3932191aeca6e0fe1abbbd4bc8f857b58c348
Qiming Fang <qf26@cornell.edu>
2015-02-04 00:58:20 -0800

    can save videos to Documents folder

--- Podfile
+++ Podfile
@@ -3,6 +3,7 @@ platform :ios, '8.0'
 inhibit_all_warnings!
 
 pod 'Facebook-iOS-SDK', '~> 3.22'
+pod 'GPUImage', '~> 0.1.6'
 pod 'ObjectiveDDP', '~> 0.1.8'
 pod 'SwipeView', '~> 1.3'
 pod 'Snap', '~> 0.0.4'
--- Serendipity/User Interface/VideoRecorderViewController.swift
+++ Serendipity/User Interface/VideoRecorderViewController.swift
@@ -7,8 +7,54 @@
 //
 
 import UIKit
+import GPUImage
 
 @objc(VideoRecorderViewController)
-class VideoRecorderViewController : BaseViewController {
+class VideoRecorderViewController : UIViewController {
+
+    @IBOutlet var recordButton: UIButton!
+    @IBOutlet weak var cameraView: GPUImageView!
+    var videoCamera : GPUImageVideoCamera?
+    var filter : GPUImageFilter?
+    var movieWriter : GPUImageMovieWriter?
     
+    var isRecording = false
+
+    // saving video to device
+    let pathToVideo = NSHomeDirectory().stringByAppendingPathComponent("Documents/video.m4v")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        videoCamera = GPUImageVideoCamera(sessionPreset: AVCaptureSessionPreset640x480, cameraPosition: .Back)
+        videoCamera!.outputImageOrientation = .Portrait;
+        
+        filter = GPUImageBrightnessFilter()
+        filter?.addTarget(cameraView)
+        
+        // set up recording mechanisms
+        let videoUrl = NSURL(fileURLWithPath: pathToVideo)
+        movieWriter = GPUImageMovieWriter(movieURL: videoUrl, size: CGSizeMake(480, 640))
+        movieWriter?.shouldPassthroughAudio = true
+        filter?.addTarget(movieWriter)
+        
+        videoCamera?.addTarget(filter)
+        videoCamera?.startCameraCapture()
+    }
+    
+    @IBAction func toggle(sender: UIButton!) {
+        if (isRecording) {
+            // stop recording
+            movieWriter?.finishRecording()
+            println("finished recording")
+            
+            // send to azure
+        } else {
+            unlink(pathToVideo) // remove any existing videos
+            movieWriter?.startRecording()
+            
+            println("recording ...")
+            isRecording = true
+        }
+    }
 }
