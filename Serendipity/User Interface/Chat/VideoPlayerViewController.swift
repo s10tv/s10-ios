24f5cf092b4166183096beeb1268bba523e927be
Tony Xiao <tony@ketch.co>
2015-02-06 10:12:21 -0800

    Move misplaced movie player from settings view to player view https://app.asana.com/0/25224911409273/26309044037598

--- Serendipity/User Interface/Chat/VideoPlayerViewController.swift
+++ Serendipity/User Interface/Chat/VideoPlayerViewController.swift
@@ -7,8 +7,29 @@
 //
 
 import UIKit
+import MediaPlayer
 
 @objc(VideoPlayerViewController)
 class VideoPlayerViewController : BaseViewController {
+    var moviePlayer : MPMoviePlayerController!
+    
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var movieView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let movieUrl = NSURL(string: "https://s10.blob.core.windows.net/s10-prod/12345/27372.m4v")!
+        moviePlayer = MPMoviePlayerController(contentURL: movieUrl)
+        moviePlayer.view.frame = CGRect(x: 0, y: 0, width: movieView.frame.width, height: movieView.frame.height)
+        
+        movieView.addSubview(moviePlayer.view)
+        moviePlayer.fullscreen = true
+    }
+    
+    @IBAction func onPlay(sender: AnyObject) {
+        moviePlayer.play()
+        
+    }
     
 }
