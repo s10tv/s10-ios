a70d6d427f0888a492ad093d103b49dee6c159fb
Tony Xiao <tony@ketch.co>
2015-02-21 23:59:56 -0800

    Update ketchIcon, adding enum definitions for glyphs in the icon font

---
+++ Ketch/Fonts/KetchIcon.swift
@@ -0,0 +1,14 @@
+//
+//  KetchIcon.swift
+//  Ketch
+//
+//  Created by Tony Xiao on 2/21/15.
+//  Copyright (c) 2015 Ketch. All rights reserved.
+//
+
+import Foundation
+
+enum KetchIcon : String {
+    case Logo = "k"
+    case TextLogo = "l"
+}
