85197a1d07cbe6cbdf362622709e9e4b849d89ca
Alex Ketch <alex@ketch.me>
2015-02-22 01:04:27 -0800

    Declared initial set of enum cases for KetchIcon font

--- Ketch/Fonts/KetchIcon.swift
+++ Ketch/Fonts/KetchIcon.swift
@@ -9,6 +9,37 @@
 import Foundation
 
 enum KetchIcon : String {
-    case Logo = "k"
-    case TextLogo = "l"
+    // Brand Elements
+    case LogoBoat = "k"
+    case LogoText = "l"
+    
+    // Decision icons
+    case MarryRound = "e"
+    case MarryIcon = "E"
+    case KeepRound = "q"
+    case KeepIcon = "Q"
+    case SkipRound = "w"
+    case SkipIcon = "W"
+    
+    // Navigation Bar Icons
+    case SettingsWheel = "y"
+    case Binoculars = "z"
+
+    // Profile View Icons
+    case Cake = "a"
+    case Briefcase = "s"
+    case GraduationCap = "d"
+    case HeightArrows = "f"
+    case Notepad = "g"
+    case LocationPin = "i"
+    case ConnectedCircles = "h"
+    
+    // Settings Page
+    case UserSilhouette = "b"
+    case Bell = "v"
+    case HeartSpeechBubble = "c"
+    case FloatRing = "x"
+    case GenderMale = "M"
+    case GenderFemale = "F"
+    case GenderBoth = "B"
 }
