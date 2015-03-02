67b509041e9891fec3772cad13e06418af8df12f
Tony Xiao <tony@ketch.co>
2015-03-01 21:47:04 -0800

    Moving Regenerate target out of xcode project into separate build script regenerate.sh

---
+++ scripts/regenerate.sh
@@ -0,0 +1,10 @@
+cd Ketch
+
+# Regenerate CoreData model files
+mogenerator -m Models/Ketch.xcdatamodeld/Ketch.xcdatamodel/ --human-dir Models --machine-dir Models/_Models --swift
+
+# Regenerate storyboard constants
+sbconstants -s Base.lproj/ -w -d > Resources/SBConstants.swift
+
+# Regenerate image and string name constants
+xcres build ../ Resources/R
