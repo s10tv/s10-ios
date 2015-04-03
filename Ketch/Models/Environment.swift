d320a9b865cbac3f86aa9968fb5d869be854463f
Tony Xiao <tony@ketch.co>
2015-04-03 16:23:50 -0700

    Use provisioning profile to configure the environment audience and use audience to configure the correct server hostname to use https://app.asana.com/0/28209080808036/30814191392951

--- Ketch/Models/Environment.swift
+++ Ketch/Models/Environment.swift
@@ -8,27 +8,48 @@
 
 import Foundation
 
-let Env = Environment()
+let Env = Environment.configureFromEmbeddedProvisioningProfile()
 
 class Environment {
     enum Audience {
         case Dev, Beta, AppStore
     }
-    let crashlyticsAPIKey = "4cdb005d0ddfebc8865c0a768de9b43c993e9113"
-    let provisioningProfile = ProvisioningProfile.embeddedProfile()
+    
     let audience : Audience
-    let serverHostName = "ketch-dev.herokuapp.com"
-//    let serverHostname = "localhost:3000"
-    let serverProtocol = "ws" // wss
-    
-    init() {
-        audience = .Dev
-    }
-    
+    let provisioningProfile : ProvisioningProfile?
+    let crashlyticsAPIKey = "4cdb005d0ddfebc8865c0a768de9b43c993e9113"
+    let serverProtocol = "ws" // TODO: Change to websocket secure
+    let serverHostName : String
     var serverURL : NSURL {
         return NSURL(string: "\(serverProtocol)://\(serverHostName)/websocket")!
     }
     var appID : String {
         return NSBundle.mainBundle().infoDictionary?["CFBundleIdentifier"] as String
+    }
+    
+    init(audience: Audience, provisioningProfile: ProvisioningProfile?) {
+        self.audience = audience
+        self.provisioningProfile = provisioningProfile
+        switch audience {
+            case .Dev:
+                serverHostName = "ketch-dev.herokuapp.com" // "localhost:3000"
+            case .Beta:
+                serverHostName = "ketch-beta.herokuapp.com"
+            case .AppStore:
+                serverHostName = "ketch.herokuapp.com"
+        }
+    }
+    
+    class func configureFromEmbeddedProvisioningProfile() -> Environment {
+        func audienceFromProfile(profile: ProvisioningProfile?) -> Audience {
+            let profileType = profile?.type ?? .AppStore
+            switch profileType {
+                case .Development:      return .Dev
+                case .Enterprise:       return .Beta
+                case .Adhoc, .AppStore: return .AppStore
+            }
+        }
+        let profile = ProvisioningProfile.embeddedProfile()
+        return Environment(audience: audienceFromProfile(profile), provisioningProfile: profile)
     }
 }
