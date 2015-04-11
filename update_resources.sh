52f0dc9ecff9a1298166399ab95ff69fa3c0be83
Tony Xiao <tony@ketch.co>
2015-04-10 23:28:56 -0700

    Moving _Models into generated resources directory

--- update_resources.sh
+++ update_resources.sh
@@ -1,7 +1,7 @@
 cd Ketch
 
 # Regenerate CoreData model files
-mogenerator -m Models/Ketch.xcdatamodeld/Ketch.xcdatamodel/ --human-dir Models --machine-dir Models/_Models --swift
+mogenerator -m Models/Ketch.xcdatamodeld/Ketch.xcdatamodel/ --human-dir Models --machine-dir Resources/_Models --swift
 
 # Regenerate storyboard constants
 sbconstants -s Base.lproj/ -w -d > Resources/SBConstants.swift
