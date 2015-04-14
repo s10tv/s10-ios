bf9dcee69daac27ff30ed92b8eb357d9d01ddc26
Tony Xiao <tony@ketch.co>
2015-04-14 12:59:34 -0700

    Adding KetchBoatView that takes care of animating itself with pitching motion

---
+++ Ketch/UserInterface/Shared/KetchBoatView.swift
@@ -0,0 +1,28 @@
+//
+//  KetchBoatView.swift
+//  Ketch
+//
+//  Created by Tony Xiao on 4/14/15.
+//  Copyright (c) 2015 Ketch. All rights reserved.
+//
+
+import Foundation
+
+class KetchBoatView : UIImageView {
+    
+    override var image : UIImage? {
+        get { return super.image }
+        set { super.image = newValue ?? UIImage(R.KetchAssets.ketch) }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.animate(keyPath: "transform.rotation") { pitch, _ in
+            pitch.fromValue = 0.2
+            pitch.toValue = -0.2
+            pitch.autoreverses = true
+            pitch.duration = 3
+            pitch.repeatCount = Float.infinity
+        }
+    }
+}
