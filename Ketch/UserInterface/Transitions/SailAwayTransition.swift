4624d298e6939120f34afef763b0e37ecea3b4ea
Tony Xiao <tony@ketch.co>
2015-04-21 00:32:43 -0700

    Fix ketch boat being occasionally upside down issue while animating sail away

--- Ketch/UserInterface/Transitions/SailAwayTransition.swift
+++ Ketch/UserInterface/Transitions/SailAwayTransition.swift
@@ -20,12 +20,10 @@ class SailAwayTransition : WaveTransition {
     }
     
     func animateBoatAway() -> RACSignal {
-        return boat.layer.animate(keyPath: "position.x") { xPos, _ in
-            xPos.byValue = self.fromView!.bounds.width
-            xPos.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
-            xPos.duration = self.boatDuration
-            xPos.fillMode = kCAFillModeForwards
-            xPos.removedOnCompletion = false
+        return UIView.animate(boatDuration, options: .CurveEaseIn) {
+            self.boat.frame.origin.x += self.fromView!.bounds.width
+        }.doCompleted {
+            self.boat.hidden = true
         }
     }
     
@@ -37,10 +35,7 @@ class SailAwayTransition : WaveTransition {
     }
     
     override func completeTransition() {
-        // Not clear to me why this is needed because after getting removed from superview
-        // boat is suppose to not have any associated animation anymore
-        self.boat.layer.removeAnimationForKey("position.x")
-        self.boat.animateAlongWave() // For some reason pitch gets removed
         super.completeTransition()
+        boat.hidden = false
     }
 }
