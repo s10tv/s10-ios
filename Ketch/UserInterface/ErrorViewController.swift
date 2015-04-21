b0057827bfef216b8c541b0508a86559d9171262
Tony Xiao <tony@ketch.co>
2015-04-20 19:39:23 -0700

    Adding error view controller, not yet hooked up to error handling

---
+++ Ketch/UserInterface/ErrorViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorViewController.swift
+//  Ketch
+//
+//  Created by Tony Xiao on 4/20/15.
+//  Copyright (c) 2015 Ketch. All rights reserved.
+//
+
+import Foundation
+
+class ErrorViewController : CloudsViewController {
+    
+}
