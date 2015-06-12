54bea9dd69c4126612c545ff08adc478fc4cd82b
Tony Xiao <tony@milasya.com>
2015-06-12 12:28:08 -0700

    Basic table view for displaying posts on home screen

--- Ketch/UserInterface/Home/HomeViewController.swift
+++ Ketch/UserInterface/Home/HomeViewController.swift
@@ -8,6 +8,31 @@
 
 import Foundation
 
-class HomeViewController : BaseViewController {
+class HomeViewController : BaseViewController,
+                           UITableViewDataSource,
+                           UITableViewDelegate {
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.registerNib(UINib(nibName: "PostHeaderView", bundle: nil), forHeaderFooterViewReuseIdentifier: "PostHeader")
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return tableView.dequeueReusableHeaderFooterViewWithIdentifier("PostHeader") as? UIView
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier("PostCell", forIndexPath: indexPath) as! UITableViewCell
+    }
+
 }
