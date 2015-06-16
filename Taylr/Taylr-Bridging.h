c8e7f4ea98a5ea4f98926c767d201eb1903db782
Tony Xiao <tony@milasya.com>
2015-06-16 09:38:04 -0700

    Fix compilation issue

--- Taylr/Taylr-Bridging.h
+++ Taylr/Taylr-Bridging.h
@@ -2,24 +2,11 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <FacebookSDK/FacebookSDK.h>
-#import <SwipeView/SwipeView.h>
-#import <SDWebImage/UIImageView+WebCache.h>
 #import <ReactiveCocoa/ReactiveCocoa.h>
 #import <Meteor/METDatabase.h>
-#import <JSQMessagesViewController/JSQMessagesViewController.h>
 //#import <BugfenderSDK/BugfenderSDK.h>
 //#import <CrashlyticsFramework/Crashlytics.h>
 #import "XLFormExtensions.h"
-
-@interface JSQMessagesViewController (Private)
-
-- (void)jsq_addObservers;
-- (void)jsq_configureMessagesViewController;
-- (void)jsq_registerForNotifications:(BOOL)registerForNotifications;
-+ (UINib *)nib;
-
-@end
 
 @interface METDatabase (Private)
 
