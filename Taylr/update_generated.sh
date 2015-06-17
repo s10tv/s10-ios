2989cf316d5a8a3792322905aafef335fb726897
Tony Xiao <tony@milasya.com>
2015-06-17 09:38:04 -0700

    Reorganizing folders

--- Taylr/update_generated.sh
+++ Taylr/update_generated.sh
@@ -1,4 +1,7 @@
-cd Taylr
+#!/bin/bash
+
+# Make sure we are in the right working directory
+cd "$(dirname "$0")"
 
 # Regenerate CoreData model files
 mogenerator -m Models/Models.xcdatamodeld/Models.xcdatamodel/ --human-dir Models --machine-dir Generated/_Models --swift
@@ -8,4 +11,4 @@ sbconstants -s Base.lproj/ -w -d > Generated/SBConstants.swift
 
 # Regenerate image and string name constants
 # Exclude generic Images.xcassets that come from including Pods resources manually
-xcres build -v -x "Pods/**" ../ Generated/R
+xcres build -v --target Taylr -x "Pods/**" ../ Generated/R
