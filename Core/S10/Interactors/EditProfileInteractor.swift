796d884d6a994da22250fdee7ee9c88cbb5d218c
Tony Xiao <tony@s10.tv>
2015-07-22 15:00:19 -0700

    Adding Me2ViewController based on an UITableViewController

--- Core/S10/Interactors/EditProfileInteractor.swift
+++ Core/S10/Interactors/EditProfileInteractor.swift
@@ -28,8 +28,8 @@ public class EditProfileInteractor {
         lastName = user.dyn("lastName").optional(String) |> map { $0 ?? "" } |> mutable
         about = user.dyn("about").optional(String) |> map { $0 ?? "" } |> mutable
         username = user.dyn("username").optional(String) |> map { $0 ?? "" }
-        avatarImageURL = user.dyn("avatarUrl").optional(String) |> map { NSURL.fromString($0) }
-        coverImageURL = user.dyn("coverUrl").optional(String) |> map { NSURL.fromString($0) }
+        avatarImageURL = user.dyn("avatar").optional(Image) |> map { $0?.url }
+        coverImageURL = user.dyn("cover").optional(Image) |> map { $0?.url }
     }
     
     public func saveEdits() -> RACFuture<(), NSError> {
---
+++ Taylr/UserInterface/Me/Me2ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  Me2ViewController.swift
+//  S10
+//
+//  Created by Tony Xiao on 7/22/15.
+//  Copyright (c) 2015 S10. All rights reserved.
+//
+
+import Foundation
+import Bond
+import ReactiveCocoa
+import Core
+
+class Me2ViewController : UITableViewController {
+    
+    @IBOutlet weak var versionLabel: UILabel!
+    @IBOutlet weak var avatarView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    var interactor: MeInteractor!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.width, height: 0.01))
+        
+        interactor = MeInteractor(meteor: Meteor, taskService: Globals.taskService, currentUser: Meteor.user.value!)
+        interactor.avatarURL ->> avatarView.dynImageURL
+        interactor.displayName ->> nameLabel
+        interactor.username ->> usernameLabel
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.contentOffset = CGPoint(x: 0, y: -66) // Totally stupid hack, donno why
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.contentInset = UIEdgeInsets(top: topLayoutGuide.length, left: 0,
+            bottom: bottomLayoutGuide.length, right: 0)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let vc = segue.destinationViewController as? ProfileViewController {
+            vc.profileVM = ProfileInteractor(meteor: Meteor, user: interactor.currentUser)
+        }
+        if let vc = segue.destinationViewController as? EditProfileViewController {
+            vc.interactor = EditProfileInteractor(meteor: Meteor, user: interactor.currentUser)
+        }
+        if let segue = segue as? LinkedStoryboardPushSegue where segue.matches(.Onboarding_Login) {
+            segue.replaceStrategy = .Stack
+        }
+    }
+    
+    // MARK: -
+    
+    @IBAction func didPressContactSupport(sender: AnyObject) {
+        let alert = UIAlertController(title: "To be implemented", message: nil, preferredStyle: .Alert)
+        alert.addAction("Ok", style: .Cancel)
+        presentViewController(alert)
+    }
+    
+    @IBAction func didPressLogout(sender: AnyObject) {
+        let sheet = UIAlertController(title: LS(.settingsLogoutTitle), message: nil, preferredStyle: .ActionSheet)
+        sheet.addAction(LS(.settingsLogoutLogout)) { _ in
+            Globals.accountService.logout()
+            self.performSegue(.Onboarding_Login, sender: self)
+        }
+        sheet.addAction(LS(.settingsLogoutCancel), style: .Cancel)
+        presentViewController(sheet)
+    }
+}
