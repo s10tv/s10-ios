48d5a17ef8f5a871470ca9b56fc92b26c489cebc
Tony Xiao <tony@s10.tv>
2015-07-30 12:08:08 -0700

    Allow the aboutfield row to automatically expand to fit content

--- Taylr/UserInterface/Onboarding/CreateProfileViewController.swift
+++ Taylr/UserInterface/Onboarding/CreateProfileViewController.swift
@@ -16,12 +16,12 @@ class CreateProfileViewController : UITableViewController {
     @IBOutlet weak var taglineField: JVFloatLabeledTextField!
     @IBOutlet weak var aboutView: JVFloatLabeledTextView!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         aboutView.floatingLabelFont = UIFont(.cabinRegular, size: 11)
         aboutView.setPlaceholder("About (Optional)", floatingTitle: "About")
         aboutView.font = UIFont(.cabinRegular, size: 16)
+        aboutView.delegate = self
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -30,3 +30,26 @@ class CreateProfileViewController : UITableViewController {
     }
 }
 
+// MARK: - Get AboutTextField's row to resize to fit content
+
+// HACK ALERT: Better way than hardcode?
+let AboutIndexPath = NSIndexPath(forRow: 3, inSection: 0)
+
+extension CreateProfileViewController : UITableViewDelegate {
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if let size = aboutView.superview?.systemLayoutSizeFittingSize(UILayoutFittingCompressedSize)
+            where indexPath == AboutIndexPath {
+                // NOTE: UITableViewAutomaticDimension doesn't work for reason not clear to me
+                return size.height
+        }
+        return super.tableView(tableView, heightForRowAtIndexPath: indexPath)
+    }
+}
+
+extension CreateProfileViewController : UITextViewDelegate {
+    func textViewDidChange(textView: UITextView) {
+        // Force tableView to recalculate the height of the textView
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+}
