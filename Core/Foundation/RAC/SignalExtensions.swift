a23e87967c527f5ef55a9fdccfbbf4a71271e41c
Tony Xiao <tony@s10.tv>
2015-08-03 14:26:23 -0700

    Add AzureClient and SignalExtensions class

---
+++ Core/Foundation/RAC/SignalExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  SignalExtensions.swift
+//  S10
+//
+//  Created by Tony Xiao on 8/3/15.
+//  Copyright (c) 2015 S10. All rights reserved.
+//
+
+import Foundation
+import ReactiveCocoa
+
+// A value that's impossible to construct
+public enum NoValue {}
+
+public func ignoreValues<T, E>(signal: Signal<T, E>) -> Signal<NoValue, E> {
+    return signal |> filter { _ in false } |> map { $0 as! NoValue }
+}
