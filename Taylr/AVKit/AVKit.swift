99aeaab196b8b9830423809ae1967b6254743b4e
Tony Xiao <tony@s10.tv>
2015-08-03 12:07:47 -0700

    Add ability to export frames from video easily RAC style

--- Taylr/AVKit/AVKit.swift
+++ Taylr/AVKit/AVKit.swift
@@ -7,13 +7,20 @@
 //
 
 import Foundation
-import SCRecorder
+import AVFoundation
 import ReactiveCocoa
 import Bond
+import SCRecorder
 import Core
 
 class AVKit {
     static let defaultFilters = AVKit.allFilters()
+    
+    class func exportFirstFrame(url: NSURL) -> Future<UIImage, NSError> {
+        let generator = AVAssetImageGenerator(asset: AVURLAsset(URL: url, options: nil))
+        generator.appliesPreferredTrackTransform = true
+        return generator.generateImages([CMTimeMake(1, 60)]) |> toFuture
+    }
     
     class func allFilters() -> [SCFilter] {
         let emptyFilter = SCFilter.emptyFilter()
@@ -29,5 +36,32 @@ class AVKit {
             SCFilter(CIFilterName: "CIPhotoEffectTransfer"),
             emptyFilter,
         ]
+    }
+}
+
+extension AVAssetImageGenerator {
+    
+    func generateImages(times: [CMTime]) -> SignalProducer<UIImage, NSError> {
+        return SignalProducer { sink, disposable in
+            let times = times.map { NSValue(CMTime: $0) }
+            var callbacksReceived = 0
+            self.generateCGImagesAsynchronouslyForTimes(times) { requestTime, image, actualTime, result, error in
+                switch result {
+                case .Succeeded:
+                    sendNext(sink, UIImage(CGImage: image)!)
+                case .Failed:
+                    sendError(sink, error)
+                case .Cancelled:
+                    sendInterrupted(sink)
+                }
+                callbacksReceived++
+                if callbacksReceived == times.count {
+                    sendCompleted(sink)
+                }
+            }
+            disposable.addDisposable {
+                self.cancelAllCGImageGeneration()
+            }
+        }
     }
 }
