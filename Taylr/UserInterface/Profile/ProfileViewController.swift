d3aa074a85eb3df9fbc4fffae31df392c6104b14
Tony Xiao <tony@s10.tv>
2015-08-29 13:22:27 -0700

    Version 0.1.1

--- Taylr/UserInterface/Profile/ProfileViewController.swift
+++ Taylr/UserInterface/Profile/ProfileViewController.swift
@@ -37,7 +37,7 @@ class ProfileViewController : BaseViewController {
             }
             return self.coverCell
         }
-        let infoSection = vm.infoVM.map { [weak self] (vm, index) -> UITableViewCell in
+        let infoSection = vm.infoVM.map { (vm, index) -> UITableViewCell in
             switch vm {
             case let vm as TaylrProfileInfoViewModel:
                 return taylrProfileFactory(vm, index)
