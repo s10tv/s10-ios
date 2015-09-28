fceb48e3d4480704ad71fcb33aefcd5bafe188a4
Tony Xiao <tony@s10.tv>
2015-09-27 23:00:52 -0700

    Create UIKitBindings for UIView, UILabel, UITextField, etc

---
+++ Core/UI/Bindings/UIKitBindings.swift
@@ -0,0 +1,62 @@
+//
+//  UIKitBindings.swift
+//  S10
+//
+//  Created by Tony Xiao on 9/27/15.
+//  Copyright © 2015 S10. All rights reserved.
+//
+
+import Foundation
+import ReactiveCocoa
+
+private var kText: UInt8 = 0
+private var kHidden: UInt8 = 0
+
+extension UIView {
+    public var rac_hidden: MutableProperty<Bool> {
+        return associatedProperty(&kHidden, setter: { [weak self] in self?.hidden = $0 }, getter: { self.hidden })
+    }
+}
+
+extension UILabel {
+    public var rac_text: MutableProperty<String?> {
+        return associatedProperty(&kText, setter: { [weak self] in self?.text = $0 }, getter: { self.text })
+    }
+}
+
+extension UITextField {
+    public var rac_text: MutableProperty<String?> {
+        return associatedObject(&kText) { [weak self] in
+            let property = MutableProperty<String?>(nil)
+            self?.addTarget(self, action: "_rac_textChanged", forControlEvents: .EditingChanged)
+            property.producer.startWithNext { [weak self] in
+                self?.text = $0
+            }
+            return property
+        }
+    }
+    
+    func _rac_textChanged() {
+        rac_text.value = self.text
+    }
+}
+
+extension UITextView {
+    public var rac_text: MutableProperty<String?> {
+        return associatedObject(&kText) { [weak self] in
+            let property = MutableProperty<String?>(nil)
+            var updatingFromSelf = false
+            self?.listenForNotification(UITextViewTextDidChangeNotification, object: self).startWithNext { [weak self] _ in
+                updatingFromSelf = true
+                property.value = self?.text
+                updatingFromSelf = false
+            }
+            property.producer.startWithNext { [weak self] in
+                if !updatingFromSelf {
+                    self?.text = $0
+                }
+            }
+            return property
+        }
+    }
+}
