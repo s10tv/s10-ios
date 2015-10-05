de5b107e370bf2025bff5f5c3f25b5c43026920d
Tony Xiao <tony@s10.tv>
2015-10-04 20:32:34 -0700

    Get rid of extraneous step in conversation tutorial

--- Taylr/UserInterface/Chats/ConversationTutorialViewController.swift
+++ Taylr/UserInterface/Chats/ConversationTutorialViewController.swift
@@ -15,21 +15,14 @@ protocol ConversationTutorialDelegate : class {
 class ConversationTutorialViewController : UIViewController {
     
     @IBOutlet weak var step1View: UIView!
-    @IBOutlet weak var step2View: UIView!
     weak var delegate: ConversationTutorialDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.clearColor()
-        step2View.hidden = true
     }
     
     @IBAction func didTapNext(sender: AnyObject) {
-        step1View.hidden = true
-        step2View.hidden = false
-    }
-    
-    @IBAction func didTapDone(sender: AnyObject) {
         delegate?.tutorialDidFinish()
     }
 }
