69f9cb703efdef37177598e9a2b14f7b7af1467f
Tony Xiao <tony@s10.tv>
2015-10-14 14:29:30 -0700

    Add more logging to VideoUpload

--- Core/S10/Operations/VideoUploadOperation.swift
+++ Core/S10/Operations/VideoUploadOperation.swift
@@ -50,14 +50,24 @@ internal class VideoUploadOperation : AsyncOperation {
             "width": width,
             "height": height
         ]
+        let prefix = "VideoUpload[\(taskId)]"
+        Log.info("\(prefix) Will Start")
+        var bgTaskId: UIBackgroundTaskIdentifier!
+        bgTaskId = UIApplication.sharedApplication().beginBackgroundTaskWithName("VideoUpload[\(taskId)]") {
+            Log.debug("\(prefix) Expired")
+            UIApplication.sharedApplication().endBackgroundTask(bgTaskId)
+        }
         meteorService.startMessageTask(taskId, recipient: recipient, info: info)
             .flatMap { res -> Future<(), NSError> in
+                Log.debug("\(prefix) Will upload to videoURL \(res.videoURL) thumbnailURL \(res.thumbnailURL)")
                 let thumb = self.azure.put(res.thumbnailURL, data: self.thumbnailData, contentType: "image/jpeg")
                 let vid = self.azure.put(res.videoURL, file: self.localURL, contentType: "video/mp4")
                 return zip(thumb.producer, vid.producer).map { _ in }.toFuture()
             }.flatMap { _ in
+                Log.debug("\(prefix) Finished uploading video & thumbnail")
                 return self.meteorService.finishTask(self.taskId)
             }.onFailure { error in
+                Log.info("\(prefix) Failed with error \(error)")
                 self.finish(.Error(error))
             }.onSuccess {
                 let realm = unsafeNewRealm()
@@ -68,6 +78,8 @@ internal class VideoUploadOperation : AsyncOperation {
                     _ = try? NSFileManager().removeItemAtURL(self.localURL)
                 }
                 self.finish(.Success)
+                Log.info("\(prefix) Finished Successfully")
+                UIApplication.sharedApplication().endBackgroundTask(bgTaskId)
             }
     }
 }
