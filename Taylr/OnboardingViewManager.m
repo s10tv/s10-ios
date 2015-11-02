07e151440b2505ce6b5f458d5bf4b4b2be10f841
Tony Xiao <tony@s10.tv>
2015-11-02 10:05:47 +0000

    Early proof of concept of TSContainerView that loads vc + view from Storyboard and plugs into React

--- Taylr/OnboardingViewManager.m
+++ Taylr/OnboardingViewManager.m
@@ -7,10 +7,52 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <React/RCTBridgeModule.h>
+#import <React/RCTViewManager.h>
+#import <React/RCTView.h>
 #import "Taylr-Swift.h"
 
-@interface OnboardingViewManager : NSObject <RCTBridgeModule>
+@interface UIView (ReactPrivate)
+
+- (void)reactAddControllerToClosestParent:(UIViewController *)controller;
+
+@end
+
+@interface TSContainerView : RCTView
+
+@property (nonatomic, strong) UIViewController *vc;
+
+@end
+
+@implementation TSContainerView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        self.vc = [[UIStoryboard storyboardWithName:@"Onboarding" bundle:nil] instantiateInitialViewController];
+//        self.vc.view.translatesAutoresizingMaskIntoConstraints = NO;
+        [self addSubview:self.vc.view];
+    }
+    return self;
+}
+
+//- (void)reactSetFrame:(CGRect)frame {
+//    self.frame = frame;
+//    self.vc.view.frame = self.bounds;
+//}
+
+//- (void)layoutSubviews {
+////    [super layoutSubviews];
+//    self.vc.view.frame = self.bounds;
+//}
+
+- (void)reactBridgeDidFinishTransaction {
+    // we can't hook up the VC hierarchy in 'init' because the subviews aren't
+    // hooked up yet, so we do it on demand here whenever a transaction has finished
+    [self reactAddControllerToClosestParent:self.vc];
+}
+
+@end
+
+@interface OnboardingViewManager : RCTViewManager
 
 @end
 
@@ -18,9 +60,16 @@
 
 RCT_EXPORT_MODULE()
 
+- (UIView *)view {
+    UIView *view = [[TSContainerView alloc] initWithFrame:CGRectZero];
+//    view.translatesAutoresizingMaskIntoConstraints = NO;
+    return view;
+}
+
+
 RCT_EXPORT_METHOD(testMethod:(NSString *)string withAlt:(NSString *)altString) {
     
-    id x = [[LoginViewController alloc] init];
+//    id x = [[LoginViewController alloc] init];
     
     NSLog(@"What's up with %@ alt: %@", string, altString);
 }
--- index.ios.js
+++ index.ios.js
@@ -10,11 +10,15 @@ var {
   StyleSheet,
   Text,
   View,
-  NativeModules
+  NativeModules,
+  MapView,
 } = React;
 
-var onboardingViewManager = NativeModules.OnboardingViewManager;
-onboardingViewManager.testMethod('Birthday Party', '4 Privet Drive, Surrey');
+var OnboardingView = React.requireNativeComponent('OnboardingView', null);
+
+
+// var onboardingViewManager = NativeModules.OnboardingViewManager;
+// onboardingViewManager.testMethod('Birthday Party', '4 Privet Drive, Surrey');
 
 var mainViewManager = NativeModules.MainViewManager;
 mainViewManager.testMethod('Some stuff', 'Some other stuff');
@@ -22,26 +26,29 @@ mainViewManager.testMethod('Some stuff', 'Some other stuff');
 
 var Taylr = React.createClass({
   render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          This is a far better development flow than most of Code
-        </Text>
-        <Text style={styles.instructions}>
-        Once someone worked out the rough edges, that's where I come in
-        Pretty sweat
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+    return (<OnboardingView style={styles.container} />)
+    // return (
+    //   <View style={styles.container}>
+    //     <Text style={styles.welcome}>
+    //       This is a far better development flow than most of Code
+    //     </Text>
+    //     <Text style={styles.instructions}>
+    //     Once someone worked out the rough edges, that's where I come in
+    //     Pretty sweat
+    //     </Text>
+    //     <Text style={styles.instructions}>
+    //       Press Cmd+R to reload,{'\n'}
+    //       Cmd+D or shake for dev menu
+    //     </Text>
+    //   </View>
+    // );
   }
 });
 
 var styles = StyleSheet.create({
   container: {
+    // width: 667,
+    // height: 375,
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
