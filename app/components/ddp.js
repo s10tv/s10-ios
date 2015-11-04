bf4cc91fa8c78632cce974af37701ac016a95a5b
Tony Xiao <tony@s10.tv>
2015-11-04 10:08:28 +0000

    @providesModule in comments is the trick, omg...

--- app/components/ddp.js
+++ app/components/ddp.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2015-present, S10, Inc.
+ *
+ * @providesModule ddp
+ * @flow
+ */
 let React = require('react-native');
 let { AsyncStorage } = React;
 let DDPClient = require("ddp-client");
@@ -83,6 +89,7 @@ ddp.call = function(methodName, params) {
 };
 
 ddp.loginWithToken = function(token) {
+  console.log("will try to login with token " + token);
   return new Promise(function(resolve, reject) {
     ddpClient.call("login", [{ resume: token }], function (err, res) {
       if (res) {
