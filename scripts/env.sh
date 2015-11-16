2f72aa8201a326957a0685ef89450489224dc8c1
Tony Xiao <tony@s10.tv>
2015-11-16 15:24:54 -0800

    Moving scripts to top level dir and out of iOS

---
+++ scripts/env.sh
@@ -0,0 +1,6 @@
+export AZURE_ACCOUNTID="s10"
+export AZURE_ACCESSKEY="wRHy5KjIvuNt+Z7WmutSm+Ms1iRHxdnh2ZqgA+2w2MuIInXCDtq2BtE6XbuJlPFggO2BCO6KmOr8PCwYHuIlqA=="
+export HEROKU_AUTH_TOKEN="20df58ba3113df0fcdbdb8df3dc2a4ad045c3964"
+export CIRCLE_BUILD_NUM=442
+export BN=442b
+export CIRCLE_ARTIFACTS="/Users/Tony/Downloads"
