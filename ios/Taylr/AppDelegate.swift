7cfea8c793dea9f615e21fd5891411122668db17
Tony Xiao <tony@s10.tv>
2015-11-24 00:43:23 -0800

    Getting rid of AppDependencies. Put all into AppDelegate for simplification

--- ios/Taylr/AppDelegate.swift
+++ ios/Taylr/AppDelegate.swift
@@ -8,33 +8,85 @@
 
 import UIKit
 import CocoaLumberjack
+import React
 import Branch
 import FBSDKCoreKit
 import NKRecorder
+import Fabric
+import DigitsKit
+import Crashlytics
+import LayerKit
+import Branch
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate /* CrashlyticsDelegate, */ {
-
+class AppDelegate : UIResponder {
     var window: UIWindow?
     
-    let dependencies = AppDependencies()
+    var env: Environment!
+    var config: AppConfig!
+    var branch: Branch!
+    var amplitude: AmplitudeProvider!
+    var mixpanel: MixpanelProvider!
+    var intercom: IntercomProvider!
+    var segment: SegmentProvider!
+    var uxcam: UXCamProvider!
+    var layer: LayerService!
+    var bridge: RCTBridge!
+    var appHubBuild: AHBuildManager!
     
-    func application(application: UIApplication, willFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
-        Analytics.track("AppOpen")
-        DDLogInfo("App Will Launch")
-        return true
-    }
-    
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        self.window?.rootViewController = RootViewController(bridge: self.dependencies.bridge)
-        self.window?.makeKeyAndVisible()
+    func setupDependencies(launchOptions: [NSObject: AnyObject]?) {
+        Crashlytics.sharedInstance().delegate = self
+        Fabric.with([Digits(), Crashlytics()])
         
-        dependencies.branch.initSessionWithLaunchOptions(launchOptions) { params, error in
+        env = Environment()
+        config = AppConfig(env: env)
+        
+        // Setup Logging
+        Logger.addLogger(DDTTYLogger.sharedInstance()) // TTY = Xcode console
+        Logger.addLogger(DDASLLogger.sharedInstance()) // ASL = Apple System Logs
+        Logger.addLogger(DDOuralabsLogger(apiKey: config.ouralabsKey))
+        Logger.addLogger(DDNSLogger())
+        
+        // Setup Analytics
+        branch = Branch.getInstance(config.branchKey)
+        amplitude = AmplitudeProvider(apiKey: config.amplitudeKey)
+        mixpanel = MixpanelProvider(apiToken: config.mixpanelToken, launchOptions: launchOptions) // TODO: Add launchOptions
+        intercom = IntercomProvider(appId: config.intercom.appId, apiKey: config.intercom.apiKey)
+        segment = SegmentProvider(writeKey: config.segmentWriteKey)
+        uxcam = UXCamProvider(apiKey: config.uxcamKey)
+        Analytics.providers = [amplitude, mixpanel, intercom, segment, uxcam]
+        
+        // Setup Layer
+        layer = LayerService(layerAppID: config.layerURL)
+        
+        // Over the air app update
+        AppHub.setApplicationID(config.appHubApplicationId)
+        appHubBuild = AppHub.buildManager()
+        appHubBuild.cellularDownloadsEnabled = true
+        appHubBuild.debugBuildsEnabled = (config.audience != .AppStore)
+        
+        // Start React Native App
+        bridge = RCTBridge(delegate: self, launchOptions: launchOptions)
+        
+    }
+}
+
+// MARK: - Application Delegate
+
+extension AppDelegate : UIApplicationDelegate {
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        setupDependencies(launchOptions)
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.rootViewController = RootViewController(bridge: bridge)
+        window?.makeKeyAndVisible()
+        
+        branch.initSessionWithLaunchOptions(launchOptions) { params, error in
             DDLogInfo("Initialized branch session params=\(params) error=\(error)")
             self.rnSendAppEvent(.BranchInitialized, body: params)
         }
-        AppHub.buildManager().fetchBuildWithCompletionHandler { build, error in
+        appHubBuild.fetchBuildWithCompletionHandler { build, error in
             DDLogInfo("Fetched new build from app hub id=\(build.identifier) name=\(build.name) desc=\(build.buildDescription) date=\(build.creationDate)")
 //            for version in build.compatibleIOSVersions { // Crashes right now...
 //                DDLogDebug("\(build.name): Compat Version - \(version)")
@@ -67,7 +119,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate /* CrashlyticsDelegate, */
         return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
     }
 
-    // MARK: - Push Handling
+    // MARK: Push Handling
     
     func application(application: UIApplication, didRegisterUserNotificationSettings notificationSettings: UIUserNotificationSettings) {
         DDLogInfo("Did register user notification settings \(notificationSettings)")
@@ -75,19 +127,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate /* CrashlyticsDelegate, */
 
     func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
         DDLogInfo("Registered for push \(deviceToken)")
-        if let apsEnv = dependencies.env.apsEnvironment?.rawValue {
+        if let apsEnv = env.apsEnvironment?.rawValue {
             // TODO: Handle push notification registration in JS
             rnSendAppEvent(.RegisteredPushToken, body: [
                 "apsEnv": apsEnv,
                 "deviceToken": deviceToken.hexString()
             ])
             Analytics.setUserProperties(["RegisteredPush": true])
-        } else if !dependencies.env.isRunningInSimulator {
+        } else if !env.isRunningInSimulator {
             DDLogError("Non-simulator build should have valid APS environment")
             // fatalError("Non-simulator build should have valid APS environment")
         }
         do {
-            try dependencies.layer.layerClient.updateRemoteNotificationDeviceToken(deviceToken)
+            try layer.layerClient.updateRemoteNotificationDeviceToken(deviceToken)
         } catch let error as NSError {
             DDLogError("Unable to update Layer with push token \(error)")
         }
@@ -100,10 +152,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate /* CrashlyticsDelegate, */
 
     func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject], fetchCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
         DDLogDebug("Did receive notification \(userInfo)")
-        // This is needed to handle remote notification while app is in the background
-        // and therefore didFinishLaunching is never invoked.
-        // TODO: LayerClient cannot be recreated, need to do again
-        let handled = dependencies.layer.layerClient.synchronizeWithRemoteNotification(userInfo) { changes, error in
+        assert(layer != nil)
+        let handled = layer.layerClient.synchronizeWithRemoteNotification(userInfo) { changes, error in
             if let error = error {
                 DDLogError("Failed to synchronize remote notification with layer \(error)")
                 completionHandler(.Failed)
@@ -125,7 +175,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate /* CrashlyticsDelegate, */
     // MARK: - Background Transfer
     
     func application(application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: () -> Void) {
-        dependencies.layer.layerClient.handleBackgroundContentTransfersForSession(identifier) { changes, error in
+        layer.layerClient.handleBackgroundContentTransfersForSession(identifier) { changes, error in
             if let error = error {
                 DDLogError("Failed to handle layer background transfer \(error)")
             } else {
@@ -133,5 +183,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate /* CrashlyticsDelegate, */
             }
             completionHandler()
         }
+    }
+}
+
+// MARK: - RCTBridgeDelegate
+
+extension AppDelegate : RCTBridgeDelegate {
+    
+    func sourceURLForBridge(bridge: RCTBridge!) -> NSURL! {
+        // Tony's Computer, uncomment for live, on-device development
+        //        return NSURL("http://192.168.0.252:8081/index.ios.bundle?platform=ios&dev=true")
+        if env.isRunningInSimulator {
+            return NSURL("http://localhost:8081/index.ios.bundle?platform=ios&dev=true")
+        } else if env.build == "0" {
+            return NSBundle.mainBundle().URLForResource("main", withExtension: "jsbundle")
+        } else {
+            let build = appHubBuild.currentBuild
+            return build.bundle.URLForResource("main", withExtension: "jsbundle")
+        }
+    }
+    
+    func extraModulesForBridge(bridge: RCTBridge!) -> [AnyObject]! {
+        return [
+            ConversationListViewManager(layer: layer),
+            ConversationViewManager(layer: layer),
+            Analytics,
+            Logger,
+            layer,
+            intercom,
+        ]
+    }
+}
+
+// MARK: - Crashlytics Delegate
+
+extension AppDelegate : CrashlyticsDelegate {
+    func crashlyticsDidDetectReportForLastExecution(report: CLSReport, completionHandler: (Bool) -> Void) {
+        // Log crash to analytics & logging
+        completionHandler(true)
     }
 }
