c30269cfe78127a1f10c8b30adaeedb66f6596dc
Tony Xiao <tony@s10.tv>
2015-11-27 03:36:08 -0800

    Separate intercom projects for prod vs dev

--- ios/Taylr/Models/AppConfig.swift
+++ ios/Taylr/Models/AppConfig.swift
@@ -30,39 +30,43 @@ class AppConfig {
         return NSURL("\(serverProtocol)://\(serverHostName)/websocket")
     }
     let uxcamKey = "2c0f24d77c8cdc6"
-    let ouralabsKey: String
-    let segmentWriteKey: String
-    let mixpanel: (projectId: String, token: String)
-    let amplitudeKey: String
-    let layerURL: NSURL
     let appHubApplicationId = "uCG85kfu67WewOZVEQBk"
+    let layerURL: NSURL
+    let ouralabsKey: String
     let branchKey: String
-    let intercom: (appId: String, apiKey: String) = (
-        appId: "q6ihw9uw",
-        apiKey: "ios_sdk-d849f0f2673a478473e25615616acf139b0e28a9"
-    )
+    let segmentWriteKey: String
+    let amplitudeKey: String
+    let mixpanel: (projectId: String, token: String)
+    let intercom: (appId: String, apiKey: String)
     
     init(audience: Audience) {
         self.audience = audience
         switch audience {
         case .Dev:
-//                serverHostName = "localhost:3000"
-//                serverHostName = "10.1.1.12:3000"
+//            serverHostName = "localhost:3000"
             serverHostName = "s10-dev.herokuapp.com"
+            layerURL = NSURL("layer:///apps/staging/49574578-72bb-11e5-9a72-a4a211002a87")
+            ouralabsKey = "2207bb0177c84bd22085cb4e9018246f"
+            branchKey = "key_test_nmeOiHF7jxXUcZPa8UdDbaacBxdYkU1J"
             segmentWriteKey = "pZimciABfGDaOLvEx9NWAFSoYHyCOg1n"
             amplitudeKey = "0ef2064f5f59aca8b1224ec4374064d3"
             mixpanel = (projectId: "773277", token: "9d5d89ba988e52622278165d91ccf937")
-            branchKey = "key_test_nmeOiHF7jxXUcZPa8UdDbaacBxdYkU1J"
-            ouralabsKey = "2207bb0177c84bd22085cb4e9018246f"
-            layerURL = NSURL("layer:///apps/staging/49574578-72bb-11e5-9a72-a4a211002a87")
+            intercom = (
+                appId: "q6ihw9uw",
+                apiKey: "ios_sdk-d849f0f2673a478473e25615616acf139b0e28a9"
+            )
         case .Beta, .AppStore:
             serverHostName = "taylr-prod.herokuapp.com"
+            layerURL = NSURL("layer:///apps/production/49574ba4-72bb-11e5-89fc-a4a211002a87")
+            ouralabsKey = "96128c67ffeb9632f665febb71914dc5"
+            branchKey = "key_live_clkHkRr6mC5Ok8Np3LeBbldcqAj8eUY4"
             segmentWriteKey = "DwMJMhxsvn6EDrO33gANHBjvg3FUsfPJ"
             amplitudeKey = "ff96d68f3ff2efd39284b33a78dbbf2c"
             mixpanel = (projectId: "671741", token: "39194eed490fa8abcc026256631a4230")
-            branchKey = "key_live_clkHkRr6mC5Ok8Np3LeBbldcqAj8eUY4"
-            ouralabsKey = "96128c67ffeb9632f665febb71914dc5"
-            layerURL = NSURL("layer:///apps/production/49574ba4-72bb-11e5-89fc-a4a211002a87")
+            intercom = (
+                appId: "nlh15x8h",
+                apiKey: "ios_sdk-730b1bc6652f4d4d6a3f3a7472d2247330cc1ad7"
+            )
         }
     }
     
