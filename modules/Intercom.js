0518e2ee4ec38a9749e0c11c0c7df6edd6722082
Qiming Fang <qf26@cornell.edu>
2015-12-03 02:58:04 -0800

    set HMAC for resume token login users

--- modules/Intercom.js
+++ modules/Intercom.js
@@ -6,8 +6,11 @@ const {
   },
 } = React;
 
+const logger = new (require('./Logger'))('Intercom');
+
 class Intercom {
   static async setHMAC(hmac, data) {
+    logger.debug(`setting hmac for userId=${data}`)
     return TSIntercomProvider.setHMAC(hmac, data);
   }
 }
