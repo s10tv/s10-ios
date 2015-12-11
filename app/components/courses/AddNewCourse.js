7420f5cab727d095ab94d8b87b5ed50a5dcb36e6
Qiming Fang <qf26@cornell.edu>
2015-12-10 23:35:08 -0800

    add infinite scroll view and search view in for courses

--- app/components/courses/AddNewCourse.js
+++ app/components/courses/AddNewCourse.js
@@ -7,22 +7,37 @@ import React, {
 } from 'react-native';
 
 import { connect } from 'react-redux/native';
-import { SHEET } from '../../CommonStyles'
-import { formatCourse } from '../courses/coursesCommon'
-import SearchBar from 'react-native-search-bar'
+import { SHEET } from '../../CommonStyles';
+import { Card } from '../lib/Card';
+import { formatCourse } from '../courses/coursesCommon';
+import AllCoursesListView from '../lib/AllCoursesListView';
+
 const logger = new (require('../../../modules/Logger'))('MyCoursesScreen')
 
 function mapStateToProps(state) {
   return {
-
+    ddp: state.ddp
   }
 }
 
 class AddNewCourse extends React.Component {
+
+  renderCourse(course) {
+    return (
+      <Card>
+        <Text>{course.dept} {course.course}</Text>
+        <Text>{course.description}</Text>
+      </Card>
+    )
+  }
+
   render() {
     return (
       <View style={SHEET.container}>
-        
+        <AllCoursesListView
+          style={{ flex: 1 }}
+          renderCourse={this.renderCourse}
+          ddp={this.props.ddp} />
       </View>
     )
   }
